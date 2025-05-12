9b0a5c14991e7f85684e3e4865153c024ca144ea
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-05-11 22:16:22 -0600

    Results

--- baseline/tokenizer.py
+++ baseline/tokenizer.py
@@ -6,6 +6,7 @@ import spacy
 from wasabi import msg
 sys.path.append('../')
 from utils.config import setConfig
+from utils.run import TokenizeText
 
 device = setConfig()
 
@@ -22,29 +23,9 @@ df['Polarity'] = df['Polarity'].astype(int)
 
 nlp = spacy.load("es_dep_news_trf")
 
-def tokenize_text(text, nlp):
-    """
-    Tokenize the input text, removing punctuation, stopwords, and converting to lowercase.
-    
-    Args:
-        text (str): The input text to tokenize.
-        nlp (spacy.Language): The spaCy language model.
-    
-    Returns:
-        list: A list of processed tokens.
-    """
-    # Procesar el texto con el modelo de spaCy
-    doc = nlp(text)
-    
-    # Extraer tokens, eliminar puntuación, stopwords y convertir a minúsculas
-    tokens = [token.text.lower() for token in doc if not token.is_stop]
-    
-    return tokens
-
-
 # Tokenize data with progress bar
-df['Title_tokens'] = df['Title'].progress_apply(lambda x: tokenize_text(x, nlp))
-df['Review_tokens'] = df['Review'].progress_apply(lambda x: tokenize_text(x, nlp))
+df['Title_tokens'] = df['Title'].progress_apply(lambda x: TokenizeText(x, nlp))
+df['Review_tokens'] = df['Review'].progress_apply(lambda x: TokenizeText(x, nlp))
 
 # Save the tokenized data to a CSV file
 
